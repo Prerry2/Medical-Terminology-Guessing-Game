9b2f237d0a030e8c531533a4ae11dd8e5ceb724a
Prerry2 <anonybe@gmail.com>
2018-08-17 02:25:10 -0400

    The whole structure will change, but I've actually made progress now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,26 @@
 // Dictionary for the hangman word choice
-var wordDictionary = []; // To be populated later
+var wordDictionary = ["abdominoplasty", "abdominal aeortic aneurysm", "acromegaly", "adenocarcinoma", "analgesia", "anemia", "anesthesia", "aneurysm", "atrophy", "bradycardia", "bronchitis", "carcinoma", "asystole", "cardiomegaly", "carditis", "cephalalgia", "cheiloschisis", "cheilosis", "cholecystectomy", "chondrodystrophy", "climacteric", "colic", "colitis", "craniotomy", "uranoplasty", "cryptorchidism", "cyanosis", "cystoplegia", "cystitis", "dehiscence", "odontalgia", "dermatitis", "diaphragm", "anaphylaxis", "hypovolemia", "hypoxia", "hyperkalemia", "hypokalemia", "hypothermia", "hypoglycemia", "hyperglycemia", "thrombosis", "thromboembolism", "epinephrine", "norepinephrine", "arrhythmia", "dysrhythmia", "ventricular fibrillation", "methylphenidate", "ibuprofen", "dyspnea", "diarrhea", "sertraline", "diazepam"]; 
+var key = event.key
 
-var wordChoice = wordDictionary[Math.floor(Math.random() * wordDictionary.length)];
-var playerInput = document.onkeyup.toLowerCase;
-// Filtering out numeric and special input to allow only lowercase letter inputs
-var letters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-var inputCheck = function() {if (letters.indexOf(playerInput) != -1) {
 
-}}
+var gameStart = function () {
+    var usedLetters = [];
+    var guessesRemaining = 8;
+
+    var wordChoice = wordDictionary[Math.floor(Math.random() * wordDictionary.length)];
+    var playerInput = document.onkeyup.toLowerCase;
+    // Filtering out numeric and special input to allow only lowercase letter inputs
+    var letters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+    var inputCheck = function () {
+        if (letters.indexOf(playerInput) != -1) {
+            // game code goes here
+            if(playerInput.indexOf(wordChoice) != -1) {
+
+            }
+            else {
+                guessesRemaining = guessesRemaining--;
+                usedLetters.unshift(key);
+            }
+        }
+    }
+}
